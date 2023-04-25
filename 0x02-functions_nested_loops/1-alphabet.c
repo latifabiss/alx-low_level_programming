5de5e27f7d5a636cd3e7a6d437b8b23ec9c49fd8
myusername <myemail@gmail.com>
2023-04-25 11:39:50 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet - prints the alphabet in lowercase.
+ *
+ * Return: always 0 (success)
+ */
+void print_alphabet(void)
+{
+	int i;
+
+	for(i = 97; i <= 122; i++)
+	{
+		putchar('i');
+	}
+	putchar('\n');
+}
